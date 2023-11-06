30e8479c16e74bf030f6c5fb91bdb5d5e9a51575
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2023-11-06 14:06:07 -0800

    Improved Homepage carousel

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function()
         slidesToShow: 1, /* Adjust the number of slides to display at once */
         slidesToScroll: 1,
         autoplay: true, /* Enable autoplay if desired */
-        autoplaySpeed: 5000, /* Adjust the autoplay speed */
+        autoplaySpeed: 3000, /* Adjust the autoplay speed */
         /* Add other carousel settings as needed */
 
         // Remove default navigation buttons
