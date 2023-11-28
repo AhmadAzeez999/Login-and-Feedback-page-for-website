9012a2add01cdd2baeae65c80d30aea109b9aee4
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2023-11-28 14:00:33 -0800

    Add files via upload

--- templates/finderScript.js
+++ templates/finderScript.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function ()
     const submitBtn = document.getElementById("btn_sub");
     const addBtn = document.getElementById("btn_add");
 
+    var gameStarted = false;
+
     // Handle the click events for each "Done" button
     for (let i = 1; i <= categories.length + 1; i++) 
     {
@@ -71,6 +73,8 @@ document.addEventListener("DOMContentLoaded", function ()
     $(btn_sub).on("click", function(e) 
     {
         e.preventDefault();
+
+        setInterval(createMissile, 2000);
 
         $(btn_sub).prop('disabled', true);
 
@@ -138,6 +142,70 @@ document.addEventListener("DOMContentLoaded", function ()
     function enableScrolling() 
     {
         $("body").css("overflow", "auto");
+    }
+
+    // Game script
+    const robot = document.getElementById('robot');
+    const gameContainer = document.getElementById('game-container');
+
+    function createMissile() 
+    {
+        console.log("creating missile");
+
+        const missile = document.createElement('div');
+        missile.className = 'missile';
+        gameContainer.appendChild(missile);
+
+        const startPosition = Math.floor(Math.random() * 4);
+        let startValue;
+
+        switch (startPosition) 
+        {
+            case 0: // Top
+                startValue = Math.random() * window.innerWidth;
+                missile.style.top = `0px`;
+                missile.style.left = `${startValue}px`;
+                break;
+            case 1: // Right
+                startValue = Math.random() * window.innerHeight;
+                missile.style.top = `${startValue}px`;
+                missile.style.left = `${window.innerWidth}px`;
+                break;
+            case 2: // Bottom
+                startValue = Math.random() * window.innerWidth;
+                missile.style.top = `${window.innerHeight}px`;
+                missile.style.left = `${startValue}px`;
+                break;
+            case 3: // Left
+                startValue = Math.random() * window.innerHeight;
+                missile.style.top = `${startValue}px`;
+                missile.style.left = `0px`;
+                break;
+        }
+
+        const robotX = robot.offsetLeft + robot.offsetWidth / 2;
+        const robotY = robot.offsetTop + robot.offsetHeight / 2;
+
+        const missileX = parseFloat(missile.style.left) + missile.offsetWidth / 2;
+        const missileY = parseFloat(missile.style.top) + missile.offsetHeight / 2;
+
+        const distance = Math.sqrt(Math.pow(robotX - missileX, 2) + Math.pow(robotY - missileY, 2));
+
+        const speed = 150; // Adjust the speed as needed
+
+        const duration = distance / speed;
+
+        missile.style.transition = `top ${duration}s linear, left ${duration}s linear`;
+        missile.style.top = `${robotY - missile.offsetHeight / 2}px`;
+        missile.style.left = `${robotX - missile.offsetWidth / 2}px`;
+
+        missile.addEventListener('click', () => {
+            missile.remove();
+        });
+
+        missile.addEventListener('transitionend', () => {
+            missile.remove();
+        });
     }
 
 });
